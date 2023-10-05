fac78735404ef583a8b2fa5e390c6defa4bb337f
manoranjanmajumdar <143195446+manoranjanmajumdar@users.noreply.github.com>
2023-10-05 17:05:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return render_template('index.html')
+def hello():
+    return "Hello, Flask with Docker!"
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=5000)
